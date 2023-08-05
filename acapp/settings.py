d32b19dab8ef0a4c5ab1435e852f61a12bbc783a
gzn <13776840962@163.com>
2023-08-05 22:14:39 +0800

    modify allow host

--- acapp/settings.py
+++ acapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-09z-p6e$+tb_q$%t)mnrbyg8bdin*+mmbfufix_nt8(%m6mw9=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["43.137.8.13"]
 
 
 # Application definition
